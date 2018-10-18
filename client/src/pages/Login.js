9ace157746d7b27348adfbcd13a3d451dec61699
Lori <lori.smith@richmond.edu>
2018-10-18 01:55:21 -0400

    modal style

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -13,20 +13,20 @@ class Login extends Component {
     return (
     <div className="hero is-fullheight has-bg-img" id="splash">
       <div className="container has-text-centered">
-      <h1 className="title" id="return">Sign In</h1>
+      <h1 className="title" id="return">SIGN IN:</h1>
       <div className="field">
       <p className="control has-text-centered">
-      <i className="fab fa-discord fa-2x"></i>  <input class="input" type="email" placeholder="Email" style={{width: 300}}></input>
+      <i className="fab fa-discord fa-2x"></i>  <input class="input" type="email" placeholder="EMAIL/USERNAME" id="emailmodal" style={{width: 300}}></input>
 
       </p>
       </div>
       <div className="field">
       <p className="control has-text-centered">
-      <i className="fas fa-lock fa-2x"></i>  <input class="input" type="password" placeholder="Password" style={{width: 300}}></input>
+      <i className="fas fa-lock fa-2x"></i>  <input class="input" type="password" placeholder="PASSWORD" id="passwordmodal" style={{width: 300}}></input>
       </p>
       </div>      
 
-      <Link to={'./dashboard'}><button class="button">Enter</button></Link>
+      <Link to={'./dashboard'}><button class="button" id="entermodal">ENTER</button></Link>
       </div>
     </div>
     );
