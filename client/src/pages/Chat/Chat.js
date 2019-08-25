367c111b129f50068ce88fbc444542fbc437f9f0
Lori <lori.smith@richmond.edu>
2019-08-25 00:52:40 -0400

    chat not available

--- client/src/pages/Chat/Chat.js
+++ client/src/pages/Chat/Chat.js
@@ -16,7 +16,8 @@ render() {
         </div>
         <br></br>
         <div className="container">
-          <iframe src="https://titanembeds.com/embed/497256090707165184" height="500" width="900" frameborder="0" id="chatroom"></iframe>
+          {/* <iframe src="https://titanembeds.com/embed/497256090707165184" height="500" width="900" frameborder="0" id="chatroom"></iframe> */}
+          Chat will return soon. We apologize for the inconvenience. :(
         </div>
       </div>
     </section>
